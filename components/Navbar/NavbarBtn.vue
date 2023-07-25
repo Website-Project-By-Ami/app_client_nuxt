8a2bf5e12f232dab9f37ab2c2f5dbc95ebf4c8ee
AureliusIvan <aurelius.ivan@student.umn.ac.id>
2023-07-25 21:51:11 +0700

    navbar

--- components/Navbar/NavbarBtn.vue
+++ components/Navbar/NavbarBtn.vue
@@ -1,10 +1,6 @@
 <!-- ivan | 11 Agustus 2023 -->
 <template>
-  <NuxtLink
-    type="button"
-    :class="active ? 'active' : ''"
-    class="button"
-    :to="to">
+  <NuxtLink type="button" :class="active ? 'active' : ''" class="button" :to="to">
     <slot />
   </NuxtLink>
 </template>
@@ -22,22 +18,22 @@ const props = defineProps({
 });
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600;700;800&display=swap");
 
 .button {
   color: #595758;
-  font-family: Noto Sans;
-  font-size: 16px;
+  font-family: 16px;
   font-style: normal;
   font-weight: 500;
   line-height: normal;
   border: none;
-  width: fit-content;
-}
+  width: fit - content;
+  font-family: 'Noto Sans', sans-serif !important;
 
-.active {
-  border-bottom: 3px solid #f3a195;
-  font-weight: 700;
+  &.active {
+    border-bottom: 3px solid #f3a195;
+    font-weight: 700;
+  }
 }
 </style>
